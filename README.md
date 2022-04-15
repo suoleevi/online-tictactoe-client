eadadc0a4454f7cbf1cf71d4c57f0c0be0f3bb9a
Leevi Suominen <100270102+suoleevi@users.noreply.github.com>
2022-04-15 11:54:55 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Client for online tictactoe web application. Uses Socket.io.
+
+Use with the backend server. 
+
+Start server with npm start.
+Start client with npm start.
---
+++ index.js
@@ -0,0 +1,88 @@
+import io from "socket.io-client";
+
+const LOCALHOST_SERVER = "http://localhost:3000";
+const socket = io(LOCALHOST_SERVER, {
+  reconnection: false,
+});
+
+const infoElem = document.getElementById("info");
+const resetElem = document.getElementById("reset");
+let homesymbol = "";
+let guestsymbol = "";
+let isTurnActive = false;
+
+// Handling received websocket messages
+
+socket.on("connect", () => {
+  infoElem.innerHTML = "Waiting for another player";
+  clearBoard();
+});
+
+socket.on("start", (data) => {
+  homesymbol = data;
+  homesymbol === "X" ? (guestsymbol = "O") : (guestsymbol = "X");
+
+  infoElem.innerHTML = "game starting, you are " + homesymbol + ", X starts";
+  resetElem.disabled = false;
+  if (data === "X") isTurnActive = true;
+});
+
+socket.on("click", (data) => {
+  console.log("click received: " + data);
+  let square = document.getElementById(data);
+  square.value = guestsymbol;
+  isTurnActive = true;
+});
+
+socket.on("win", (data) => {
+  infoElem.innerHTML = "You won!";
+  isTurnActive = false;
+});
+
+socket.on("lose", (data) => {
+  infoElem.innerHTML = "You lost!";
+  isTurnActive = false;
+});
+
+socket.on("tie", (data) => {
+  infoElem.innerHTML = "It's a tie!";
+  isTurnActive = false;
+});
+
+socket.on("disconnected", (data) => {
+  infoElem.innerHTML = "Other player disconnect, find a new game";
+  isTurnActive = false;
+});
+
+socket.on("reset", () => {
+  clearBoard();
+  infoElem.innerHTML = "game starting, you are " + homesymbol + ", X starts";
+  if (homesymbol === "X") isTurnActive = true;
+});
+
+// Defining functions
+
+window.clicked = function clicked(id) {
+  let square = document.getElementById(id);
+  if (!isTurnActive || square.value) return;
+  socket.emit("click", id);
+  square.value = homesymbol;
+  isTurnActive = false;
+};
+
+window.resetGame = function resetGame() {
+  socket.emit("reset");
+};
+
+window.newGame = function newGame() {
+  socket.disconnect();
+  socket.connect();
+  resetElem.disabled = true;
+};
+
+function clearBoard() {
+  const board = document.getElementsByClassName("box");
+  Array.from(board).forEach((square) => {
+    square.value = "";
+  });
+}
